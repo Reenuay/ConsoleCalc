a8761a47e31f16d0008463bd1a38cffe1fbc2c26
Reenuay <reenuay777@gmail.com>
2020-03-24 17:39:41 +0400

    Add logic in main function

--- Expression.fs
+++ Expression.fs
@@ -1,4 +1,4 @@
-module ConsoleCalc
+namespace ConsoleCalc
 
 open System
 
--- Program.fs
+++ Program.fs
@@ -1,7 +1,22 @@
 module ConsoleCalc.Progam
 
 open System
+open FParsec
 
 [<EntryPoint>]
 let main _ =
-    0 // return an integer exit code
+  printfn "Welcome to console calc!"
+
+  let rec iter () =
+    printfn "Please enter a new expression:"
+
+    let input = Console.ReadLine()
+    match Parser.parse input with
+    | Success (e, _, _) -> Expression.solve e |> printfn "Result: %f"
+    | Failure (e, _, _) -> printfn "Error: %s" e
+
+    iter ()
+
+  iter ()
+  
+  0 // return an integer exit code
